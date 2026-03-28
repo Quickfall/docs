b6c6fb24b260a4e7c59ac209e719a3d65699fac1
Toby <83095177+tobezdev@users.noreply.github.com>
2026-03-28 22:44:55 +0000

    Simple homepage for now + config

---
+++ docmd.config.js
@@ -0,0 +1,80 @@
+// docmd.config.js
+export default defineConfig({
+  // --- Core ---
+  title: 'My Documentation',
+  url: 'https://quickfall-docs.tobezdev.com/',
+
+  // --- Branding ---
+  logo: {
+    light: 'assets/images/quickfall_logo_full_dark_transparent.png',
+    dark: 'assets/images/quickfall_logo_full_dark_transparent.png',
+    alt: 'Logo',
+    href: '/',
+  },
+  favicon: 'assets/images/favicon.ico',
+
+  // --- Source & Output ---
+  src: 'docs',
+  out: 'site',
+
+  // --- Layout & UI Architecture ---
+  layout: {
+    spa: true,
+    header: {
+      enabled: true,
+    },
+    sidebar: {
+      collapsible: false,
+      defaultCollapsed: false,
+    },
+    optionsMenu: {
+      position: 'menubar',
+      components: {
+        search: true,      
+        themeSwitch: false, 
+        sponsor: 'https://github.com/sponsors/Quickfall',     
+      }
+    },
+    footer: {
+      style: 'minimal',
+      content: '&copy; ' + new Date().getFullYear() + ' Quickfall. All rights reserved.',
+      branding: false
+    }
+  },
+
+  // --- Theme Settings ---
+  theme: {
+    name: 'quickfall',
+    appearance: 'dark',
+    codeHighlight: true,    
+    customCss: ['assets/images/static/fixedFooter.css'],          
+  },
+
+  // --- General Features ---
+  minify: true,           
+  autoTitleFromH1: true,  
+  copyCode: true,         
+  pageNavigation: true,   
+  
+  customJs: [],           
+
+
+  // --- Navigation (Sidebar) ---
+  navigation: [
+    { title: 'Home', path: '/', icon: 'home' },
+  ],
+
+  // --- Plugins ---
+  plugins: {
+    seo: { aiBots: false },
+    sitemap: { defaultChangefreq: 'weekly' },
+    analytics: { googleV4: { measurementId: '' } },
+  },
+  
+  // --- Edit Link ---
+  editLink: {
+    enabled: false,
+    baseUrl: 'https://github.com/quickfall/docs/edit/main/docs/',
+    text: 'Contribute to the Docs!'
+  }
+});
---
+++ docs/index.md
@@ -0,0 +1,16 @@
+---
+title: "Quickfall Docs"
+description: "The documentation page for the Quickfall programming language."
+---
+
+# Welcome to the Quickfall Documentation
+
+This documentation will cover the basics of Quickfall, including the schemas it uses, code structure and the cool tips you can use to make developmemt easier. If this isn't what you're looking for, try the links below.
+
+- <a href="https://github.com/Quickfall/language-server" target="_blank" rel="noopener noreferrer">Language Server</a>
+- <a href="https://github.com/Quickfall/resources" target="_blank" rel="noopener noreferrer">Branding Resources</a>
+- <a href="https://github.com/Quickfall" target="_blank" rel="noopener noreferrer">Github</a>
+- ~~<a href="" target="_blank" rel="noopener noreferrer">Donate</a>~~
+
+With that covered, if you still can't find what you're looking for, you can ask in [our Discord server]() or [email Support]().
+
